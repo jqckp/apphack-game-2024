9bada23d21f8376cd15274ff2d925e842429f770
Oscar Silva-Santiago <oscarrsilvaa04@gmail.com>
2024-03-23 15:32:53 -0400

    character movement beginning

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ pygame.init()
 
 FPS = 60
 
-WIZARD = pygame.image.load(os.path.join('Assets', 'Player_Character.png'))
-WIZARD = pygame.transform.scale(WIZARD, (100, 100))
+COWBOY = pygame.image.load(os.path.join('Assets', 'Player_Character.png'))
+COWBOY = pygame.transform.scale(COWBOY, (50, 60))
+COWBOY_X = 300
+COWBOY_Y = 100
+COWBOY_speed = 10
 
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT = 1000, 1000
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Spaghetti Fantasy")
 
 def make_game():
-    WIN.blit(WIZARD, (300, 100))
-    pygame.display.flip()
+    WIN.fill((0,0,0))
+    WIN.blit(COWBOY, (COWBOY_X, COWBOY_Y))
+    pygame.display.update()
 
 clock = pygame.time.Clock()
 running = True
@@ -25,6 +29,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        COWBOY_X -= COWBOY_speed
+        COWBOY = pygame.transform.flip(COWBOY, True, False)
+    if keys[pygame.K_RIGHT]:
+        COWBOY_X += COWBOY_speed
+        COWBOY = pygame.transform.flip(COWBOY, True, False)
+    if keys[pygame.K_UP]:
+        COWBOY_Y -= COWBOY_speed
+    if keys[pygame.K_DOWN]:
+        COWBOY_Y += COWBOY_speed
+
+    COWBOY_X = max(0, min(COWBOY_X, WIDTH - COWBOY.get_width()))
+    COWBOY_Y = max(0, min(COWBOY_Y, HEIGHT - COWBOY.get_height()))
     make_game()
 
 pygame.quit()
