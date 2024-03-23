95ad332bc1675045d299ad0085d7e216c39a1bb3
Oscar Silva-Santiago <oscarrsilvaa04@gmail.com>
2024-03-23 15:00:37 -0400

    Game Window Progress

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import pygame
 import sys
+import os
 
 pygame.init()
 
-WIDTH, HEIGHT = 800, 600
+FPS = 60
+
+WIZARD = pygame.image.load(os.path.join('Assets', 'wizard.png'))
+WIZARD = pygame.transform.scale(WIZARD, (100, 100))
+
+WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Spaghetti Fantasy")
 
+def make_game():
+    WIN.blit(WIZARD, (300, 100))
+    pygame.display.flip()
+
+clock = pygame.time.Clock()
 running = True
 while running:
-
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    make_game()
 
 pygame.quit()
 sys.exit()
