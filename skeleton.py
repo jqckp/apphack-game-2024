6947af880b6339a121aa82b943ab59a99810f22e
Jack <porterjp1@appstate.edu>
2024-03-23 19:15:27 -0400

    Add skeleton stats

--- skeleton.py
+++ skeleton.py
@@ -1,3 +1,7 @@
-projectile_speed = 5 #Projectile speed
+projectile_speed = 5 #Projectile speed (slowest of the three)
 damage = 3 #1.5 hearts of damage
+projectile_size = (10, 10) #Largest projectile size of the three
+shots_to_kill = 3 #A skeleton takes 3 bullets to kill (most out of the three)
+
+
 
