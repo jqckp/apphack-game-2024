832e7710b381abbe0a672d21c60e9d02ba05c31c
Oscar Silva-Santiago <oscarrsilvaa04@gmail.com>
2024-03-23 15:36:22 -0400

    character movement again

--- main.py
+++ main.py
@@ -6,38 +6,20 @@ pygame.init()
 
 FPS = 60
 
-<<<<<<< HEAD
 COWBOY = pygame.image.load(os.path.join('Assets', 'Player_Character.png'))
 COWBOY = pygame.transform.scale(COWBOY, (50, 60))
 COWBOY_X = 300
 COWBOY_Y = 100
 COWBOY_speed = 10
-=======
-
-COWBOY = pygame.image.load(os.path.join('Assets', 'Player_Character.png'))
-COWBOY = pygame.transform.scale(COWBOY, (100, 100))
-
-
-COWBOY_POSITION = pygame.Rect(425, 225, 100, 100)
->>>>>>> acf7ec176d8e9435e7b59fb26467e7f4538e709d
 
 WIDTH, HEIGHT = 1000, 1000
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Spaghetti Fantasy")
 
-<<<<<<< HEAD
 def make_game():
     WIN.fill((0,0,0))
     WIN.blit(COWBOY, (COWBOY_X, COWBOY_Y))
     pygame.display.update()
-=======
-
-
-
-def display_frame(COWBOY_POSITION):
-    WIN.blit(COWBOY, (COWBOY_POSITION.x, COWBOY_POSITION.y))
-    pygame.display.flip()
->>>>>>> acf7ec176d8e9435e7b59fb26467e7f4538e709d
 
 clock = pygame.time.Clock()
 running = True
@@ -47,7 +29,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-<<<<<<< HEAD
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         COWBOY_X -= COWBOY_speed
@@ -63,9 +44,6 @@ while running:
     COWBOY_X = max(0, min(COWBOY_X, WIDTH - COWBOY.get_width()))
     COWBOY_Y = max(0, min(COWBOY_Y, HEIGHT - COWBOY.get_height()))
     make_game()
-=======
-    display_frame(COWBOY_POSITION)
->>>>>>> acf7ec176d8e9435e7b59fb26467e7f4538e709d
 
 pygame.quit()
 sys.exit()
