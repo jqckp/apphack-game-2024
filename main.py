2ef9c6408699f782d5a5b987d08fff3e2812567f
Jack <porterjp1@appstate.edu>
2024-03-23 14:44:40 -0400

    Limit fps to 60

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import sys
 
 pygame.init()
 
+FPS = 60
+
+clock = pygame.time.Clock()
+
 WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Spaghetti Fantasy")
@@ -10,6 +14,7 @@ pygame.display.set_caption("Spaghetti Fantasy")
 running = True
 while running:
 
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
