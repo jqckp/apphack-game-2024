75b93790a6599b8b2bb7355d5cc6614c96745c49
Jack <porterjp1@appstate.edu>
2024-03-23 22:30:17 -0400

    Add skeleton class and constructor to generate skeletons

--- skeleton.py
+++ skeleton.py
@@ -1,7 +1,28 @@
-projectile_speed = 5 #Projectile speed (slowest of the three)
-damage = 3 #1.5 hearts of damage
-projectile_size = (10, 10) #Largest projectile size of the three
-shots_to_kill = 3 #A skeleton takes 3 bullets to kill (most out of the three)
+import pygame
+import os
+import random
 
+
+
+
+class skeleton:
+
+    
+    def __init__(self):
+        self.x_coord = random.randint(0, 600)
+        self.y_coord = random.randint(0, 600)
+        self.SKELETON = pygame.image.load(os.path.join('Assets', 'Skeleton_Enemy.png'))
+        self.SKELETON = pygame.transform.scale(self.SKELETON, (75, 75))
+        self.SKELETON_POSITION = self.SKELETON.get_rect(center = (self.x_coord, self.y_coord))
+        self.projectile_speed = 5
+        self.damage = 3
+        self.projectile_size = (10, 10)
+        self.shots_to_kill = 3
+        self.health = 3
+        
+
+
+    
+    
 
 
